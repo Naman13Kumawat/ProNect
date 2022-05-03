b74eb95df78f795656e44cc6bb4f2096ff70bb2d
nikhil123456b <bbalaswamy303@gmail.com>
2022-05-03 15:44:40 +0530

    add quote

---
+++ pronect/src/components/Quote/Quote.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Row, Card, Col } from "react-bootstrap";
+import styles from "./Quote.module.css";
+
+export default function Quote() {
+  return (
+    <div className={`${styles.Quote}`}>
+      <Container fluid>
+        <h1>IF YOU LIKE TO SPONSOR CURRENT EVENTS, CALL US!</h1>
+       
+      </Container>
+    </div>
+  );
+}
