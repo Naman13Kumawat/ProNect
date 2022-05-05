6e1ee6c8cbf4b269f96357654e6b7d76d6a7a73e
nikhil123456b <bbalaswamy303@gmail.com>
2022-05-05 19:14:07 +0530

    add filter text

--- pronect/src/components/EventConnectPage/EventCP.jsx
+++ pronect/src/components/EventConnectPage/EventCP.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./EventCP.module.css"
 import Navbar from "./Navbar/Navbar"
+import Filter from './Filter/Filter';
 import Footer from "../Home/Footer/Footer"
 
 export default function EventCP(){  
     return (
     <>
         <Navbar />
+        <h1>Events</h1>
+        <h4>Filters</h4>
+        <Filter />
+        <p> </p>
         <Footer />
     </>
   )
